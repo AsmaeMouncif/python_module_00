966a9912a749054458e123611dd47696fcc6c8a5
MOUNCIF ASMAE <158608762+AsmaeMouncif@users.noreply.github.com>
2026-01-16 14:20:00 +0000

    version final 01

--- ex2/ft_harvest_total.py
+++ ex2/ft_harvest_total.py
@@ -1,19 +1,17 @@
-# **************************************************************************** #
-#                                                                              #
-#                                                         :::      ::::::::    #
-#    ft_harvest_total.py                                :+:      :+:    :+:    #
-#                                                     +:+ +:+         +:+      #
-#    By: asmounci <asmounci@student.42.fr>          +#+  +:+       +#+         #
-#                                                 +#+#+#+#+#+   +#+            #
-#    Created: 2026/01/14 13:05:14 by asmounci          #+#    #+#              #
-#    Updated: 2026/01/14 13:11:45 by asmounci         ###   ########.fr        #
-#                                                                              #
-# **************************************************************************** #
+# *************************************************************************** #
+#                                                                             #
+#                                                       :::      ::::::::    #
+#    ft_harvest_total.py                             :+:      :+:    :+:    #
+#                                                   +:+ +:+         +:+      #
+#    By: asmounci <asmounci@student.42.fr>        +#+  +:+       +#+         #
+#                                               +#+#+#+#+#+   +#+            #
+#    Created: 2026/01/14 13:05:14 by asmounci      #+#    #+#              #
+#    Updated: 2026/01/14 13:11:45 by asmounci     ###   ########.fr        #
+#                                                                             #
+# *************************************************************************** #
 
 def ft_harvest_total():
     day1 = int(input("Day 1 harvest: "))
-    day2 = int(input("Day 2 harvest: ")) 
+    day2 = int(input("Day 2 harvest: "))
     day3 = int(input("Day 3 harvest: "))
     print("Total harvest:", day1 + day2 + day3)
-
-ft_harvest_total()
--- ex5/ft_count_harvest_recursive.py
+++ ex5/ft_count_harvest_recursive.py
@@ -1,22 +1,24 @@
-# **************************************************************************** #
-#                                                                              #
-#                                                         :::      ::::::::    #
-#    ft_count_harvest_recursive.py                      :+:      :+:    :+:    #
-#                                                     +:+ +:+         +:+      #
-#    By: asmounci <asmounci@student.42.fr>          +#+  +:+       +#+         #
-#                                                 +#+#+#+#+#+   +#+            #
-#    Created: 2026/01/14 17:23:56 by asmounci          #+#    #+#              #
-#    Updated: 2026/01/14 17:35:02 by asmounci         ###   ########.fr        #
-#                                                                              #
-# **************************************************************************** #
+# *************************************************************************** #
+#                                                                             #
+#                                                       :::      ::::::::    #
+#    ft_count_harvest_recursive.py                    :+:      :+:    :+:    #
+#                                                   +:+ +:+         +:+      #
+#    By: asmounci <asmounci@student.42.fr>        +#+  +:+       +#+         #
+#                                               +#+#+#+#+#+   +#+            #
+#    Created: 2026/01/14 17:23:56 by asmounci      #+#    #+#              #
+#    Updated: 2026/01/14 17:35:02 by asmounci     ###   ########.fr        #
+#                                                                             #
+# *************************************************************************** #
 
-days = int(input("Days until harvest: "))
-
-def ft_count_harvest_recursive(n):
-    if n > days:
-        print("Harvest time!")
-    else:
-        print("Day",n)
-        ft_count_harvest_recursive(n + 1)
+def ft_count_harvest_recursive():
+    days = int(input("Days until harvest: "))
     
-ft_count_harvest_recursive(1)
+    def ft_recursive(n):
+        if n > days:
+            print("Harvest time!")
+        else:
+            print("Day", n)
+            ft_recursive(n + 1)
+    
+    ft_recursive(1)
+ft_count_harvest_recursive()
