259a9c1817c1a0ba928285d7dac34d0cd84dd201
MOUNCIF ASMAE <158608762+AsmaeMouncif@users.noreply.github.com>
2026-01-17 10:40:49 +0000

    version final

--- ex1/ft_plot_area.py
+++ ex1/ft_plot_area.py
@@ -1,17 +1,5 @@
-# *************************************************************************** #
-#                                                                             #
-#                                                       :::      ::::::::    #
-#    ft_plot_area.py                                 :+:      :+:    :+:    #
-#                                                   +:+ +:+         +:+      #
-#    By: asmounci <asmounci@student.42.fr>        +#+  +:+       +#+         #
-#                                               +#+#+#+#+#+   +#+            #
-#    Created: 2026/01/14 12:31:40 by asmounci      #+#    #+#              #
-#    Updated: 2026/01/14 13:02:11 by asmounci     ###   ########.fr        #
-#                                                                             #
-# *************************************************************************** #
-
 def ft_plot_area():
     length = int(input("Enter length: "))
     width = int(input("Enter width: "))
-    
+
     print("Plot area:", length * width)
